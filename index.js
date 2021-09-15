b3be79758928d84dc2fb751b86bce4d05ed8db64
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-09-15 10:40:38 +0200

    Update index.js
    Force true

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db');
 
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ alter: true })
+conn.sync({ force: true })
     .then(() => {
         console.log('ok');
         server.listen(PORT, () => {
