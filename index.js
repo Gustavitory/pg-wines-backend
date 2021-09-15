92b7efa06a636ff0fee239a322e5c005e816659c
Gaspar Almada <almada3d@gmail.com>
2021-09-15 12:41:01 -0300

    uploading changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db');
 
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ alter: true })
+conn.sync({ alter: true })  // Esto evita que se borren datos al reiniciarse el servidor.
     .then(() => {
         server.listen(PORT, () => {
             console.log(`Server listening at ${PORT}`);
