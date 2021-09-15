f1c9122f05c35e666b290e795142c9282e94c978
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-09-15 03:05:25 +0200

    fix6

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: true })
     .then(() => {
+        console.log('ok');
         server.listen(PORT, () => {
             console.log(`Server listening at ${PORT}`);
         });
