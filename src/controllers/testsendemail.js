098744bf90d468416d8ea69c693733a7a6162076
Gaspar Almada <almada3d@gmail.com>
2021-09-16 13:43:13 -0300

    feat: send email to client

---
+++ src/controllers/testsendemail.js
@@ -0,0 +1,4 @@
+const { sendEmail } = require('./sendEmail');
+
+// El proposito de este archivo es el de solo para ejecutarlo en desarrollo para probar el funcionamiento del controller 'sendEmail.js'
+sendEmail('Gaspar Almada', 'almada3d@gmail.com', false);
