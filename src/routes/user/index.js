0b98eb705c52df8f84b3a5ff9116e2b05eb87232
emilianoduartes <emilianoduartes@gmail.com>
2021-09-17 17:40:50 -0300

    fix: controller user.js

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 
-const { getAllUsers, updateUser, deleteUser, loginUser, newUser } = require('../../controllers/user/user')
+const { getAllUsers, updateUser, deleteUser, loginUser, newUser, getUserByEmail } = require('../../controllers/user/user')
 
 
 const router = Router();
 
 router.get('/', getAllUsers);
 router.get('/login', loginUser);
-router.put('/:idUser', updateUser); 
-router.delete('/:idUser', deleteUser); 
+router.get('/byemail', getUserByEmail);
+router.put('/', updateUser); 
+router.delete('/:id', deleteUser); 
 router.post('/register',newUser);
 
 module.exports = router;
