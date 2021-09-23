21f9efbf1dd8a5390cbca90c0c97e7967a7c9f31
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-09-23 07:39:25 -0500

    fix: forgotPassword.js and sendEmail.js => error sending an email

--- src/controllers/sendEmail.js
+++ src/controllers/sendEmail.js
@@ -11,7 +11,8 @@ async function sendEmail(clientName, clientEmail, reason, link) {  // Si el terc
 
     // Controlo que la razon del email sea una razon válida.
     if (!['purchase', 'delivery', 'reset-password'].includes(reason)) {
-        return res.status(404).send({ error: 'The reason parameter is invalid' });
+        // return res.status(404).send({ error: 'The reason parameter is invalid' });
+        return { error: 'The reason parameter is invalid' }
     }
 
     // Datos para el login de la cuenta emisora del email, es decir, la cuenta de email de Bodegas Del Sur.
@@ -68,10 +69,12 @@ async function sendEmail(clientName, clientEmail, reason, link) {  // Si el terc
     // Envío el Email.
     await transporter.sendMail(mailStructure, (error, info) => {
         if (error) {
-            res.status(500).send(error.message);
+            // res.status(500).send(error.message);
+            console.log("Error in transporter.sendMail: "+error.message);
         } else {
             console.log('The Email was sent!');
-            res.status(200).json(info);
+            // res.status(200).json(info);
+            console.log(JSON.stringify(info));
         }
     })
 };
